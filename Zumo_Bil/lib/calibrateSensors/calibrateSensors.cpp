a9e578617fc471ed4635e55947b25d1aed1d16ff
Undal <krisunda@stud.ntnu.no>
2025-11-06 15:35:50 +0100

    opprette kalibreringsfil

---
+++ Zumo_Bil/lib/calibrateSensors/calibrateSensors.cpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <Zumo32U4.h>
+#include "calibrateSensors.h"
+
+Zumo32U4LineSensors lineSensors;
+
---
+++ Zumo_Bil/lib/calibrateSensors/calibrateSensors.h
@@ -0,0 +1,5 @@
+#ifndef __CALIBRATE_SENSORS__
+#define __CALIBRATE_SENSORS__
+
+
+#endif
