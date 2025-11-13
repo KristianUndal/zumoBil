5226432d0b529f1b3cd60326cf8f20d9ac0b0762
Vegard Hult <vegardhult@gmail.com>
2025-11-13 12:23:23 +0100

    Separat display fil

--- Zumo_Bil/lib/display/display.cpp
+++ Zumo_Bil/lib/display/display.cpp
@@ -4,4 +4,15 @@
 
 Zumo32U4OLED display;
 
+void clearScreen() {
+    // Clear screen
+    display.clear();
+}
 
+void writeToScreen(String str, int line) {
+    // Write to specific line
+    display.gotoXY(0, line);
+    
+    // Print a batteryCPercentage to screen
+    display.print(str);
+}
--- Zumo_Bil/lib/display/display.h
+++ Zumo_Bil/lib/display/display.h
@@ -1,6 +1,13 @@
-#ifndef DISPLAY
-#define DISPLAY
+#ifndef __DISPLAY__
+#define __DISPLAY__
 
+#include <Arduino.h>
+#include <Zumo32U4.h>
 
+// Clear screen
+void clearScreen();
 
-#endif
+// Write string to screen
+void writeToScreen(String str, int line);
+
+#endif // __DISPLAY__
